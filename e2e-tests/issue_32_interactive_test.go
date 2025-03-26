bd79d56776819e2f702a1ff1dd41392c6660822d
Maxim Slipenko <no-reply@maxim.slipenko.com>
2025-03-26 10:22:56 +0300

    fix: add interactive=false handling in remove command

---
+++ e2e-tests/issue_32_interactive_test.go
@@ -0,0 +1,40 @@
+// ALR - Any Linux Repository
+// Copyright (C) 2025 Евгений Храмов
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build e2e
+
+package e2etests_test
+
+import (
+	"testing"
+
+	"github.com/alecthomas/assert/v2"
+	"github.com/efficientgo/e2e"
+)
+
+func TestE2EIssue32Interactive(t *testing.T) {
+	dockerMultipleRun(
+		t,
+		"issue-32-interactive",
+		COMMON_SYSTEMS,
+		func(t *testing.T, r e2e.Runnable) {
+			err := r.Exec(e2e.NewCommand(
+				"alr", "--interactive=false", "remove", "ca-certificates",
+			))
+			assert.NoError(t, err)
+		},
+	)
+}
